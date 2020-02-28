5d8be6d1937256cbb5804c4036a5485bae63d7ec
EliedersonLinhares <eliederson250@gmail.com>
2020-02-28 10:01:51 -0300

    Projeto criado

---
+++ src/test/java/com/esl/blogmongo/BlogmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esl.blogmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
